481fbf6a19a6e2635b9c787695c330e7b09fa68c
bigtorresg <bigtorresg@gmail.com>
2023-08-15 18:02:26 +0200

    deployment configs

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-96k!h$g3-jb-1vmhm-=kwetc!%#5=%0^tlovlnw)k)r7w)*ybo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
